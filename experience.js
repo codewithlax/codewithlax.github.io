73785b3008cd8b941e8b6ad686683851b6089ff3
Lakshmanan Sivakumar <133585102+codewithlax@users.noreply.github.com>
2024-10-08 22:58:32 -0400

    Update experience.js

--- experience.js
+++ experience.js
@@ -12,3 +12,10 @@ function toggleDetails(companyId) {
     // Toggle the clicked company's details
     details.style.display = (details.style.display === 'block') ? 'none' : 'block';
 }
+
+
+// Scroll to the experience section when "Explore My Work" is clicked
+document.getElementById('explore-button').addEventListener('click', function() {
+    const experienceSection = document.getElementById('experience-section');
+    experienceSection.scrollIntoView({ behavior: 'smooth' });
+});
